8307637ac627f68a8620a6204ce1c04b2d31dc86
Thysie <mjvw.skippy@gmail.com>
2019-11-21 16:52:07 +0200

    EOD

--- Android/Gen/build/generated/source/buildConfig/debug/com/mycompany/ts_broadcast/BuildConfig.java
+++ Android/Gen/build/generated/source/buildConfig/debug/com/mycompany/ts_broadcast/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.mycompany.ts_broadcast";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 10;
-  public static final String VERSION_NAME = "0.0.10.0";
+  public static final int VERSION_CODE = 36;
+  public static final String VERSION_NAME = "0.0.14.0";
 }
