5f5a6768089662526c14791319a0f60efe315470
Thysie <mjvw.skippy@gmail.com>
2019-11-28 14:21:49 +0200

    promotions commit

--- Android/Gen/build/generated/source/buildConfig/debug/com/mycompany/ts_broadcast/BuildConfig.java
+++ Android/Gen/build/generated/source/buildConfig/debug/com/mycompany/ts_broadcast/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.mycompany.ts_broadcast";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 80;
-  public static final String VERSION_NAME = "0.0.52.0";
+  public static final int VERSION_CODE = 125;
+  public static final String VERSION_NAME = "0.0.96.0";
 }
