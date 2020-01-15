02db72bf1ae3d503057907e5ff74ff6096b83ec5
Thysie <mjvw.skippy@gmail.com>
2020-01-15 15:12:37 +0200

    2019 Final Commit

--- Android/Gen/build/generated/source/buildConfig/debug/com/mycompany/ts_broadcast/BuildConfig.java
+++ Android/Gen/build/generated/source/buildConfig/debug/com/mycompany/ts_broadcast/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.mycompany.ts_broadcast";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 167;
-  public static final String VERSION_NAME = "0.0.134.0";
+  public static final int VERSION_CODE = 168;
+  public static final String VERSION_NAME = "0.0.135.0";
 }
